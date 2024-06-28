b4af2851e7b89b571943b521bdc5bb0c3173abd9
vasiliy.prokopenko <madwazz1@gmail.com>
2024-06-28 18:09:40 +0300

    Add poetry files and main.py

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
