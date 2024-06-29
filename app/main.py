6d541a99b4cd89c2125f60f7c4b9593d94adeed9
vasiliy.prokopenko <madwazz1@gmail.com>
2024-06-29 20:29:00 +0300

    Add drafts of first 2 methods

--- app/main.py
+++ app/main.py
@@ -1,11 +1,50 @@
-from fastapi import FastAPI
+from dataclasses import dataclass
+from datetime import date
+
+from fastapi import FastAPI, Query, Depends
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@dataclass
+class HotelSearchArgs:
+    location: str
+    date_from: date
+    date_to: date
+    has_spa: bool = None
+    stars: int = Query(default=None, ge=1, le=5)
+
+
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+@app.get("/hotels", response_model=list[SHotel])
+async def get_hotels(
+        search_args: HotelSearchArgs = Depends()
+):
+    hotels = [
+        {
+            "address": "ул. Гагарина, 1, Алтай",
+            "name": "Super Hotel",
+            "stars": 5
+        }
+    ]
+    return hotels
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+async def add_booking(booking: SBooking):
+    pass
 
 
 if __name__ == "__main__":
